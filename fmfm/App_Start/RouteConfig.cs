cdd678706ee78493dc0cc483b8241069620d3936
Fedor Predkozka <lomidrevo@gmail.com>
2016-11-29 15:37:27 +0100

    playlist view

--- fmfm/App_Start/RouteConfig.cs
+++ fmfm/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace fmfm
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Playlist", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
